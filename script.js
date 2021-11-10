88a4b901f85a0c8c6207c073cd3cb39b16d4961f
Csilla Pósa <posa.csilla@gmail.com>
2021-11-10 14:58:03 +0100

    Setting up so we get only the joke as a string
    In the JSON file, the joke is under data.setup and data.delivery (https://sv443.net/jokeapi/v2/) so through those properties we can extract the joke as a string.

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const audioElement = document.getElementById('audio');
 // Get Jokes from Joke API
 
 async function getJokes() {
+    let joke = '';
     const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        console.log(data);
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+        console.log(joke);
     } catch (error) {
         console.log("whoops", error)
     }
